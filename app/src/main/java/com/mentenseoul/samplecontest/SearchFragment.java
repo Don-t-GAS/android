b122d5bfb00a3ad6808035e1207e8c6eaff2f3b5
mac <mentenseoul@gmail.com>
2021-05-21 18:40:32 +0900

    initial commit

---
+++ app/src/main/java/com/mentenseoul/samplecontest/SearchFragment.java
@@ -0,0 +1,47 @@
+package com.mentenseoul.samplecontest;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SearchFragment extends Fragment {
+    EditText searchText;
+    String modelName;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        searchText = view.findViewById(R.id.searchText);
+
+        searchText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                modelName = searchText.getText().toString();
+                Toast.makeText(view.getContext(), "test:" + modelName, Toast.LENGTH_SHORT).show();
+//                text = editText.getText().toString();
+//                new NoticeJsoup().execute();
+                return true;
+            }
+        });
+    }
+
+
+}
